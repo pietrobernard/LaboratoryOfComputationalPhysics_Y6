b6c7189e1b47e8b7865defa1f5f896843cbbce67
Pietro Bernardi <pietro.bernardi@studenti.unipd.it>
2023-10-12 09:41:00 +0200

    correction of exercise 8#

--- 00ex/nested.py
+++ 00ex/nested.py
@@ -21,21 +21,36 @@ poi lui fa: for b in x:
     [0,1,2]
         0, 1, 2
 
+(3,4,5), (4,5,3), (5,3,4)
+ma anche: (3,5,4) e (5,4,3) e (4,3,5)
+ho 6 possibili permutazioni, infatti 3!
+
+
+
 """
 
 def rotate(l, n):
     return l[n:] + l[:n]
 
 def perm(in_tuple):
-    # calculates all the permutations
+    # calculates all the 6 permutations for a given triple
     perms = [rotate(in_tuple, x) for x in range(0,len(in_tuple),1)]
+    n_tuple = (in_tuple[0],in_tuple[2],in_tuple[1])
+    perms += [rotate(n_tuple, x) for x in range(0,len(n_tuple),1)]
     # then i should return the 'fundamental' that I decide to be the one with the smallest number first
     # the fundamental it is checked if it's already in the 'triples'
     perms.sort() #sorted(perms, key = lambda x: (x[0], x[1], x[2]))
     return perms[0]
 
 triples = [(x,y,z) for x in range(1,100) for y in range(1,100) for z in range(1,100) if (x**2+y**2==z**2)]
-unique_triples = [triple for triple in triples if perm(triple) not in triples]
+
+unique_triples = []
+triples_rep = {}
+for i in triples:
+    triple_rep = perm(i)
+    if triple_rep not in triples_rep:
+        triples_rep[triple_rep] = 0
+        unique_triples.append(triple_rep)
 
 print(triples)
 print(unique_triples)
